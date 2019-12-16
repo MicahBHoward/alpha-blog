2c683931790aa2ceb7f1e2facdfd0a90274a08d5
Micah Howard <micahbhoward@me.com>
2019-12-16 08:42:01 -0500

    Complete authentication system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,8 +12,8 @@ def logged_in?
 end
 
 def require_user
-	if !logged_in
-		flash[:danger] = "You must be logged in to perform that action."
+	if !logged_in?
+		flash[:danger] = "You must be logged in to perform this action."
 		redirect_to root_path
 	end
 end
